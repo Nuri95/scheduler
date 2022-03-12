e7d51ca2734862ac7ae9e683d83113294f182648
Nuri <nurhotun95@mail.ru>
2022-03-12 15:31:06 +0500

    git ignore добавила

--- src/scheduler/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from scheduler.api.auth import auth_router
-from scheduler.api.tasks import tasks_router
-
-router = APIRouter()
-router.include_router(auth_router)
-router.include_router(tasks_router)
